ae0a81858c0163902e5f13189fedb7dd702d5d3b
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-11-15 02:13:11 -0500

    1.1.0

--- Private/Functions.ps1
+++ Private/Functions.ps1
@@ -1,1 +1,68 @@
-x
+function Get-GpoCommentFile {
+    <#
+    .SYNOPSIS
+    Get Gpo Comment File
+    
+    .DESCRIPTION
+    Get GPO comment file path
+    
+    .PARAMETER Gpo
+    Group Policy Object reference
+    
+    .EXAMPLE
+    $filepath = Get-GpoCommentFile -Gpo $GPO
+    
+    .NOTES
+    
+    .INPUTS
+        Group Policy Object reference
+    .OUTPUTS
+        Path and Name of Comment CMTX file
+    #>
+    param (
+        [parameter(Mandatory=$True, HelpMessage="Group Policy ID")]
+        [ValidateNotNullOrEmpty()]
+        $Gpo
+    )
+    $result = "\\$($gpo.DomainName)\SYSVOL\$($gpo.DomainName)\Policies\{$($gpo.ID)}\$config\comment.cmtx"
+    if (Test-Path $result) {
+        Write-Output $result
+    }
+    else {
+        Write-Output ""
+    }
+}
+
+function Get-GppCommentPath {
+    <#
+    .SYNOPSIS
+    Get Group Policy Preferences Comment file path
+    
+    .DESCRIPTION
+    Get Group Policy Preferences Comment file path
+    
+    .PARAMETER Gpo
+    Group Policy Object reference
+    
+    .EXAMPLE
+    $gppPath = Get-GppCommentPath -Gpo $gpo
+    
+    .NOTES
+    General notes
+    #>
+    param (
+        [parameter(Mandatory=$True, HelpMessage="Group Policy Object")]
+        [ValidateNotNullOrEmpty()]
+        $Gpo,
+        [parameter(Mandatory=$True, HelpMessage="Computer or User")]
+        [ValidateNotNullOrEmpty()]
+        [string] $ConfigType
+    )
+    $result = "\\$($gpo.DomainName)\SYSVOL\$($gpo.DomainName)\Policies\{$($gpo.Id)}\$ConfigType\Preferences"
+    if (Test-Path $result) {
+        Write-Output $result
+    }
+    else {
+        Write-Output ""
+    }
+}
